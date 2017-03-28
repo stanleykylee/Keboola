937bdadf4414c0112e146f17f788877ef9cf8d7a
Stanley Lee <stanley.ky.lee@gmail.com>
2017-03-27 18:23:49 -0700

    EDA on payload

--- extractData.py
+++ extractData.py
@@ -1,8 +1,32 @@
 import requests
 
-resp = requests.get('https://api.typeform.com/v1/forms?key=ac83034cfa742c0f79c26e9a612b4ba7e2aa0d3d')
+apiUrl = "https://api.typeform.com/v1/"
+apiKey = "ac83034cfa742c0f79c26e9a612b4ba7e2aa0d3d"
+
+# get all forms available to apiKey
+resp = requests.get(apiUrl + "forms?key=" + apiKey)
 if resp.status_code != 200:
     # This means something went wrong.
     raise Exception
-for item in resp.json():
-    print(item)
+
+# accessing and getting a list of forms on account
+for form in resp.json():
+    print(form['id'])
+
+# using the first form for exploratory data analysis
+formId = resp.json()[0]['id']
+resp = requests.get(apiUrl + "form/" + formId + "?key=" + apiKey)
+if resp.status_code != 200:
+    # This means something went wrong.
+    raise Exception
+
+payload = resp.json()
+# explore payload
+print(payload)
+
+# explore questions and answers
+for question in payload['questions']:
+    print(question)
+
+for response in payload['responses']:
+    print(response)
