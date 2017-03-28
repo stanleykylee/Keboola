72b5fe7c3f3c7c4548241f6144cd670f72ad6ba9
Stanley Lee <stanley.ky.lee@gmail.com>
2017-03-27 20:31:40 -0700

    added comments and cleaned up debug lines

--- extractData.py
+++ extractData.py
@@ -1,3 +1,22 @@
+#!/usr/bin/env python
+"""
+Title:          extractData.py
+Description:    Extracts data from Typeform form via API requests.
+Author:         Stanley Lee
+Date:           March 27, 2017
+Version:        0.1
+Usage:          python extractData.py
+Python Version: 3.5.3
+
+This python script will connect to Typeform via API requests to gather user entered form responses. It requires the
+API url and API key to access the data. The output of the python script will be a csv formatted file named after the
+form where the data is sourced form. Any missing response data is replaced with 'NaN' in the output file.
+
+Todo:
+    * Add option to provide API url via command line
+    * Add option to provide API key via command line
+    * Add option to specify which form to extract data
+"""
 import requests
 import csv
 
@@ -12,59 +31,47 @@ if resp.status_code != 200:
 
 # accessing and getting a list of forms on account
 for form in resp.json():
-    print(form['id'])
 
-# using the first form for exploratory data analysis
-formId = resp.json()[0]['id']
-resp = requests.get(apiUrl + "form/" + formId + "?key=" + apiKey)
-if resp.status_code != 200:
-    # This means something went wrong.
-    raise Exception
+    # grab data from current form
+    resp = requests.get(apiUrl + "form/" + form['id'] + "?key=" + apiKey)
+    if resp.status_code != 200:
+        # This means something went wrong.
+        raise Exception
 
-payload = resp.json()
-# explore payload
-print(payload)
+    # gather payload and generate fieldnames from payload
+    payload = resp.json()
+    fieldnamesIds = []
+    fieldnamesIdsMap = {}
 
-# explore questions and answers
-fieldnamesIds = []
-fieldnamesIdsMap = {}
-for question in payload['questions']:
-    print(question)
-    fieldnamesIds.append(question['id'])
-    fieldnamesIdsMap[question['id']] = question['question'].replace(u'\xa0', u' ')
+    for question in payload['questions']:
+        fieldnamesIds.append(question['id'])
+        fieldnamesIdsMap[question['id']] = question['question'].replace(u'\xa0', u' ')
 
-print(fieldnamesIds)
-print(fieldnamesIdsMap)
-print(fieldnamesIds[0])
-print(fieldnamesIdsMap[fieldnamesIds[0]])
+    # create out CSV with the name of the current form
+    with open(form['id'] + '.csv', 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile, delimiter=',')
 
-for response in payload['responses']:
-    print(response)
-
-# create CSV
-with open(formId + '.csv', 'w', newline='') as csvfile:
-    writer = csv.writer(csvfile, delimiter=',')
-
-    # write csv header
-    header = [fieldnamesIdsMap[fieldnamesIds[0]]]
-    i = 1
-    while i < len(fieldnamesIds):
-        header.append(fieldnamesIdsMap[fieldnamesIds[i]])
-        i += 1
-    writer.writerow(header)
-
-    for response in payload['responses']:
-        try:
-            responseRow = [response['answers'][fieldnamesIds[0]]]
-        except KeyError:
-            responseRow = ['NaN']
+        # write csv header
+        header = [fieldnamesIdsMap[fieldnamesIds[0]]]
         i = 1
         while i < len(fieldnamesIds):
-            try:
-                responseRow.append(response['answers'][fieldnamesIds[i]])
-            except KeyError:
-                responseRow.append('NaN')
+            header.append(fieldnamesIdsMap[fieldnamesIds[i]])
             i += 1
+        writer.writerow(header)
 
-        print(responseRow)
-        writer.writerow(responseRow)
+        # write response data, replacing missing keys with NaN
+        for response in payload['responses']:
+            try:
+                responseRow = [response['answers'][fieldnamesIds[0]]]
+            except KeyError:
+                responseRow = ['NaN']
+            i = 1
+            while i < len(fieldnamesIds):
+                try:
+                    responseRow.append(response['answers'][fieldnamesIds[i]])
+                except KeyError:
+                    responseRow.append('NaN')
+                i += 1
+            writer.writerow(responseRow)
+
+    print("Typeform form data from UID {} has been created as {}.csv".format(form['id'], form['id']))
